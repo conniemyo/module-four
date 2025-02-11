9e13e9ceb83a08f8a6434f456420ab96a81f8919
conniemyo <connieyoungxo@gmail.com>
2025-02-11 12:57:47 -0600

    changes

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ function askQuestion(question, correctAnswer) {
         document.write("<p>" + question + " Correct!</p>");
         return 1;
     } else {
-        document.write("<p>" + question + " The correct answer is..." + correctAnswer + "</p>");
+        document.write("<p>" + question + " The correct answer was ... " + correctAnswer + "</p>");
         return 0;
     }
 }
@@ -30,16 +30,16 @@ score += askQuestion("What is the square root of 16?", 4);
 let feedback;
 switch (score) {
     case 3:
-        feedback = "You Scored...3/3!";
+        feedback = "Your Score Is . . . 3/3!";
         break;
     case 2:
-        feedback = "You Scored...2/3!";
+        feedback = "Your Score Is . . . 2/3!";
         break;
     case 1:
-        feedback = "You Scored...1/3!";
+        feedback = "Your Score Is . . . 1/3!";
         break;
     default:
-        feedback = "You Scored...0/3!";
+        feedback = "Your Score Is . . . 0/3!";
 }
 
 document.write("<h3>" + feedback + "</h3>");
